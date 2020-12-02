1ca47a1b06eabbdacb9f3ef68161bae1ef697956
Suhas K Shetty <57042304+DarkSchokolade@users.noreply.github.com>
2020-12-03 00:02:12 +0530

    express update

--- nodejs testing/app.js
+++ nodejs testing/app.js
@@ -4,18 +4,24 @@ const app = express()
 
 app.use(express.json())
 
-// http.createServer(function (req, res) {
-//     res.writeHead(200, { 'Content-Type': 'text/html' });
-//     res.write('AVS-RFID-TECH: ');
-//     res.write(req.url)
-//     res.end();
-// }).listen(8080);
+app.get('/', (req, res) =>{
+    console.log(req.query)
+    res.send(req.query) // displays query string
+})
 
 app.get('/:id',(req,res)=>{
-    res.send(req.params)
+    res.send(req.params) // url id stuff
 })
 
 const port = process.env.PORT || 3000
 app.listen(port, ()=>{
     console.log(`server is starting at ${port}`)
 })
+
+
+// http.createServer(function (req, res) {
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+//     res.write('AVS-RFID-TECH: ');
+//     res.write(req.url)
+//     res.end();
+// }).listen(8080);
