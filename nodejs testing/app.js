976874d1ab22e7b56128f89c6419c42069473704
Suhas K Shetty <57042304+DarkSchokolade@users.noreply.github.com>
2020-12-02 23:45:23 +0530

    added express

--- nodejs testing/app.js
+++ nodejs testing/app.js
@@ -1,8 +1,21 @@
 const http = require('http');
+const express = require('express')
+const app = express()
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('AVS-RFID-TECH: ');
-    res.write(req.url)
-    res.end();
-}).listen(8080);
+app.use(express.json())
+
+// http.createServer(function (req, res) {
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+//     res.write('AVS-RFID-TECH: ');
+//     res.write(req.url)
+//     res.end();
+// }).listen(8080);
+
+app.get('/:id',(req,res)=>{
+    res.send(req.params)
+})
+
+const port = process.env.PORT || 3000
+app.listen(port, ()=>{
+    console.log(`server is starting at ${port}`)
+})
